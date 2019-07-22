81f69b776b95c566cec8b2e62ff4038d50e87a50
Cameron Steele <Cam.steeleis@gmail.com>
2019-07-22 18:50:49 -0500

    DOM wait

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,71 +1,69 @@
-var clicker = 0;
-var images = ["photosphere.jpg"]
-var viewer = new PhotoSphereViewer({
-    container: 'viewer',
-    panorama: './dist/img/' + images[0],
-    navbar: [
-        'autorotate',
-        'zoom',
-        'fullscreen'
-    ]
-});
-
-$('#slide-next').click(function(){
-    viewer.setPanorama('./dist/img/' + images[++clicker % images.length]);
-})
-
-$('#slide-prev').click(function(){
-    viewer.setPanorama('./dist/img/' + images[--clicker % images.length]);
-})
-
-// Smooth scrolling using jQuery easing
-$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-            $('html, body').animate({
-                scrollTop: (target.offset().top - 54)
-            }, 1000, "easeInOutExpo");
-            return false;
-        }
-    }
-});
-
-// Closes responsive menu when a scroll trigger link is clicked
-$('.js-scroll-trigger').click(function () {
-    $('.navbar-collapse').collapse('hide');
-});
-
-// Activate scrollspy to add active class to navbar items on scroll
-$('body').scrollspy({
-    target: '#mainNav',
-    offset: 56
-});
-
-// Collapse Navbar
-var navbarCollapse = function () {
-    if ($("#mainNav").offset().top > 100) {
-        $("#mainNav").addClass("navbar-shrink");
-    } else {
-        $("#mainNav").removeClass("navbar-shrink");
-    }
-};
-// Collapse now if page is not at top
-navbarCollapse();
-// Collapse the navbar when page is scrolled
-$(window).scroll(navbarCollapse);
-
-// Hide navbar when modals trigger
-$('.portfolio-modal').on('show.bs.modal', function (e) {
-    $(".navbar").addClass("d-none");
-})
-$('.portfolio-modal').on('hidden.bs.modal', function (e) {
-    $(".navbar").removeClass("d-none");
-})
-
-
 document.addEventListener("DOMContentLoaded", function () {
+    var clicker = 0;
+    var images = ["photosphere.jpg"]
+    var viewer = new PhotoSphereViewer({
+        container: 'viewer',
+        panorama: './dist/img/' + images[0],
+        navbar: [
+            'autorotate',
+            'zoom',
+            'fullscreen'
+        ]
+    });
+
+    $('#slide-next').click(function () {
+        viewer.setPanorama('./dist/img/' + images[++clicker % images.length]);
+    })
+
+    $('#slide-prev').click(function () {
+        viewer.setPanorama('./dist/img/' + images[--clicker % images.length]);
+    })
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 54)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function () {
+        $('.navbar-collapse').collapse('hide');
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: 56
+    });
+
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+
+    // Hide navbar when modals trigger
+    $('.portfolio-modal').on('show.bs.modal', function (e) {
+        $(".navbar").addClass("d-none");
+    })
+    $('.portfolio-modal').on('hidden.bs.modal', function (e) {
+        $(".navbar").removeClass("d-none");
+    })
     var buttonText_small = {
         today: "Today",
         month: "M",
@@ -96,7 +94,7 @@ document.addEventListener("DOMContentLoaded", function () {
     };
     var calendarEl = document.getElementById('calendar');
 
-    var calendar = new FullCalendar.Calendar( calendarEl, {
+    var calendar = new FullCalendar.Calendar(calendarEl, {
         plugins: [
             'dayGrid',
             'googleCalendar',
@@ -119,9 +117,9 @@ document.addEventListener("DOMContentLoaded", function () {
         },
         eventClick: function (info) {
             info.jsEvent.preventDefault();
-            
+
         },
-        eventRender: function(){
+        eventRender: function () {
             let events = calendar.getEvents();
             console.log(events);
             events.forEach(event => {
@@ -136,8 +134,8 @@ document.addEventListener("DOMContentLoaded", function () {
     })
 
     calendar.render();
-    
-    
+
+
 
     $(document).resize(function () {
         var width = $(document).width();
