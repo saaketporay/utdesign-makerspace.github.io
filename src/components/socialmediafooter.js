6a9e024f73f26a911f0e9c35d1f9563990cfbb96
Cameron Steele <Cam.steeleis@gmail.com>
2019-08-06 19:16:27 -0500

    Added Social Medias

--- src/components/socialmediafooter.js
+++ src/components/socialmediafooter.js
@@ -6,22 +6,22 @@ const SocialMediaFooter = () => {
     return (
         <ul className="list-inline social-buttons">
             <li className="list-inline-item">
-                <a href="https://twitter.com" className="twitter-btn">
+                <a href="https://twitter.com/utdmakerspace" className="twitter-btn">
                     <FontAwesomeIcon icon={faTwitter} />
                 </a>
             </li>
             <li className="list-inline-item">
-                <a href="https://instagram.com" className="instagram-btn">
+                <a href="https://www.instagram.com/utdmakerspace/" className="instagram-btn">
                     <FontAwesomeIcon icon={faInstagram} />
                 </a>
             </li>
             <li className="list-inline-item">
-                <a href="https://facebook.com" className="facebook-btn">
+                <a href="https://facebook.com/utdmakerspace" className="facebook-btn">
                     <FontAwesomeIcon icon={faFacebookF} />
                 </a>
             </li>
             <li className="list-inline-item">
-                <a href="https://linkedin.com/" className="linkedin-btn"><FontAwesomeIcon icon={faLinkedinIn} /></a>
+                <a href="https://www.linkedin.com/company/40749650" className="linkedin-btn"><FontAwesomeIcon icon={faLinkedinIn} /></a>
             </li>
         </ul>
     )
