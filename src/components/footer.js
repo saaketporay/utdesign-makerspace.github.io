f474183804abdae159bb9181a9c66e749de7a3ee
Cameron Steele <cam_steele@ymail.com>
2020-01-17 16:50:43 -0600

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,6 +11,7 @@ const Footer = ({ children }) => {
                 <div className="row align-items-center">
                     <div className="col-md-4">
                         <span className="copyright">Copyright &copy; UTDesign Makerspace {new Date().getFullYear()}</span>
+                        <span>UTDMaker.space is published by The UTDesign Makerspace and is not an offical publication of UT Dallas and does not represent the view of the university or its officers</span>
                     </div>
                     <div className="col-md-4">
                         <SocialMediaFooter />
